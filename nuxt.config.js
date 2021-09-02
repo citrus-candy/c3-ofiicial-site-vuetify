ae4b87863f5b7641346e497096015c2a63bd8901
citrus-candy <kyuuri11013@gmail.com>
2021-09-02 23:41:49 +0900

    [change] Site Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from "vuetify/es5/util/colors";
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: "%s - c3-offical-site-vuetify",
+    titleTemplate: "%s",
     title: "c3-offical-site-vuetify",
     htmlAttrs: {
       lang: "ja"
